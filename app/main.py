4e7d41b1f65cdfd8f74bc8a841a134295fe0f1f4
Yahia Salman <yahia.salman.04@gmail.com>
2025-04-26 13:13:47 -0400

    adding outline

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from byaldi import RAGMultiModalModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+
+
+
+# Brendan API's go here
+########################################################
+
+
+
+
+
+########################################################
